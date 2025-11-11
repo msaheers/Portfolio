77dfd6e112410ec4dbfd611749bd2988f8816160
msaheers <mishalsaheersha10@gmail.com>
2025-11-11 08:54:35 +0000

    disable eslint build blocking for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // ⛔ Ignore ESLint errors during Vercel build
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true, // (Optional) if you’re using normal <img> instead of <Image />
+  },
+};
 
 export default nextConfig;
